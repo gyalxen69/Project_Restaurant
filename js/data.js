df667ff644dfa16e0f203f916d6ed3e95d032213
gyalxen69 <blak.curry@gmail.com>
2022-02-09 11:49:53 +0100

    starting

---
+++ js/data.js
@@ -0,0 +1,12 @@
+const food = [
+   bhat = {
+       nombre: 'Dhal Bhat',
+       precio: 10,
+       descipcion: 'Arros, lentejas'
+   },
+   momo = {
+       nombre: 'Mo:Mo',
+       precio: 8,
+       descipcion: 'Herina, Carne, mezclado con verduras'
+   }
+]
---
+++ js/script.js
@@ -0,0 +1,1 @@
+import { food } from './data.js' 
