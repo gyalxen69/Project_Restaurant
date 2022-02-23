f7ba74fa663239c348ee323ced0777d009de55cc
gyalxen69 <blak.curry@gmail.com>
2022-02-23 13:27:48 +0100

    until the js

--- js/data.js
+++ js/data.js
@@ -1,12 +1,28 @@
 const food = [
-   bhat = {
-       nombre: 'Dhal Bhat',
-       precio: 10,
-       descipcion: 'Arros, lentejas'
-   },
-   momo = {
-       nombre: 'Mo:Mo',
-       precio: 8,
-       descipcion: 'Herina, Carne, mezclado con verduras'
-   }
+    {
+       name: 'Dhal Bhat',
+       price: '$10',
+       ingridients: 'Rice, Lentils, Curry, Pickle',
+       description: 'Fine size of plate filled with rice, lentils,curry, pickle and the melted butter on top'
+    },
+    {
+       name: 'Mo:Mo',
+       price: '$8',
+       ingridients: 'Flour, Meat, Mix of vegetables',
+       description: 'hi'
+    },
+    {
+        name: 'Samosa',
+        price: '$7',
+        ingridients: 'Flour, Potato mixed with Peas and Onions',
+        description: 'hi'
+    },
+    {
+        name: 'Chicken Curry',
+        price: '$9',
+        ingridients: 'Pollo and some Garnished with some herbs',
+        description: 'A special curry of chicken meat.'
+    }
 ]
+
+export { food };
--- js/script.js
+++ js/script.js
@@ -1,1 +1,47 @@
 import { food } from './data.js' 
+console.log(food);
+let counter = 0;
+
+function initialize(){
+    addingListeners();
+    printHtml(counter);
+}
+
+initialize();
+
+function printHtml(counter){
+    const foodName = document.querySelector('.food-name');
+    const ingri = document.querySelector('.ingri');
+    const descri = document.querySelector('.food-descri');
+    const price = document.querySelector('.price');
+    foodName.innerText = food[counter].name;
+    ingri.innerText = food[counter].ingridients;
+    descri.innerText = food[counter].description;
+    price.innerText = food[counter].price;
+}
+
+function addingListeners(){
+    const btnNext = document.querySelector('.btn-next');
+    btnNext.addEventListener('click', forNext);
+    const btnBack = document.querySelector('.btn-back');
+    btnBack.addEventListener('click', forBack)
+}
+
+function forNext(){
+    counter += 1;
+    if(counter > food.length - 1)
+    {
+        counter = 0;
+    }
+    printHtml(counter);
+}
+
+function forBack(){
+    counter -= 1;
+    if(counter < 0)
+    {
+        counter = food.length - 1;
+    }
+    printHtml(counter);
+}
+
